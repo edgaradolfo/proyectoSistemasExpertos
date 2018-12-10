a805b8a89fd728cfa27097c4251b7403098a03b9
edgaradolfo <40681838+edgaradolfo@users.noreply.github.com>
2018-12-10 13:08:30 -0800

    Add files via upload

---
+++ js/Scroll.js
@@ -0,0 +1,22 @@
+(function($) {
+    "use strict";
+
+    var $navbar = $("#mainNav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "-20px"
+            }, 0);
+        }
+    });
+
+})(jQuery, undefined);
